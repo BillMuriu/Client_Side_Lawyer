aa212be01ebeec657b0b66b81b1680fbfb37c14d
unknown <njorogekamau63@gmail.com>
2023-03-03 12:54:19 +0300

    updated experiment

--- scripts.js
+++ scripts.js
@@ -1,65 +1,18 @@
-    // sidebar toggle
-    let btn = document.querySelector("#btn");
-    let sidebar = document.querySelector(".sidebar");
+const menuActive = document.getElementById("menu_active");
+const menuClosed = document.getElementById("menu_closed");
+const sideBar = document.querySelector(".side_bar");
+const content = document.querySelector(".content");
 
-    btn.onclick = function(){
-        sidebar.classList.toggle("active");
-    };
+menuActive.addEventListener("click", () => {
+  sideBar.style.left = "0";
+  menuActive.style.display = "none";
+  menuClosed.style.display = "block";
+  content.style.filter = "blur(5px)";
+});
 
-
-    // sticky sidebar
-    const side_bar = document.querySelector('.sidebar');
-    const sidebarTop = sidebar.offsetTop;
-
-    window.addEventListener('scroll', () => {
-    if (window.pageYOffset >= sidebarTop) {
-        side_bar.style.position = 'fixed';
-        side_bar.style.top = 0;
-    } else {
-        side_bar.style.position = 'relative';
-    }
-    });
-
-
-    // sticky top bar
-    const top_bar = document.querySelector('.top_bar');
-    const topbarTop = top_bar.offsetTop;
-
-    window.addEventListener('scroll', () => {
-    if (window.pageYOffset >= topbarTop) {
-        top_bar.style.position = 'fixed';
-        top_bar.style.top = 0;
-    } else {
-        top_bar.style.position = 'sticky';
-        top_bar.style.top = null;
-    }
-    });
-
-
-
-    const selectBtns = document.querySelectorAll('.select-btn');
-
-    selectBtns.forEach(selectBtn => {
-    selectBtn.addEventListener('click', () => {
-        selectBtn.classList.toggle('open');
-    });
-
-    const items = selectBtn.parentNode.querySelector('.list-items').querySelectorAll('.item');
-
-    items.forEach(item => {
-        item.addEventListener('click', () => {
-        item.classList.toggle('checked');
-
-        let checked = selectBtn.parentNode.querySelector('.list-items').querySelectorAll('.checked'),
-            btnText = selectBtn.querySelector('.btn-text');
-
-        if (checked && checked.length > 0) {
-            btnText.innerText = `${checked.length} Selected`;
-        } else {
-            btnText.innerText = 'Select Language';
-        }
-        });
-    });
-    });
-
-    
+menuClosed.addEventListener("click", () => {
+  sideBar.style.left = "-200px";
+  menuActive.style.display = "block";
+  menuClosed.style.display = "none";
+  content.style.filter = "none";
+});
